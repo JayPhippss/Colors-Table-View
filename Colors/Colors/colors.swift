ea8512c23b7755df0dbc28568843ac083f36eb66
JayPhippss <PhippsJaylin@gmail.com>
2017-07-18 19:06:53 -0500

    Add Background colors to match the text in the table view

---
+++ Colors/Colors/colors.swift
@@ -0,0 +1,14 @@
+//
+//  colors.swift
+//  Colors
+//
+//  Created by Jaylin Phipps on 7/18/17.
+//  Copyright © 2017 Jaylin Phipps. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
